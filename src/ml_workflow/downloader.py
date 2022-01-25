48fce5ac427dc58350a1ce234e7fcb0ce726582f
Victoria Oberascher <S1810824017@students.fh-hagenberg.at>
2022-01-25 16:19:48 +0100

    add nested cross validation

--- src/ml_workflow/downloader.py
+++ src/ml_workflow/downloader.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import new_figure_manager
 import spotipy, json, os
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
@@ -15,6 +16,7 @@ def __read_file(filename: str) -> dict:
             data = json.load(f)
         return data
     except:
+        
         print("No such file!")
         return None
 
@@ -39,7 +41,16 @@ def get_track_IDs_from_playlist_ID(playlist_ID: str) -> list:
         playlist = sp.playlist_items(playlist_ID, offset=offset)
         total_tracks -= 100
         offset += 100
-        id_list = [i["track"]["id"] for i in playlist["items"]]
+        playlist["items"] = list(filter(partial(is_not, None), playlist["items"]))
+
+        id_list = []
+        for item in playlist["items"]:
+            try:
+                id_list.append(item["track"]["id"])
+            except: pass
+
+        #id_list = [i["track"]["id"] for i in playlist["items"]]
+
         ids += list(set(id_list))
         
     # return a list of all track ids without None values
@@ -78,40 +89,60 @@ def extract_features_from_playlist(playlist_ID: str, genre: str, filename: str =
 def extract_features_from_multiple_playlists(playlist_dict: dict) -> None:
     for genre, playlist_list in playlist_dict.items():
         track_ID_list = []
+        features = list()
         for playlist_ID in playlist_list:
             track_IDs = get_track_IDs_from_playlist_ID(playlist_ID)
             track_ID_list += track_IDs
+
         features = get_features_from_track_IDs(track_ID_list)
+        features = list(filter(partial(is_not, None), features))
         features = [dict(f, **{"genre": genre}) for f in features]
+
         __write_file(genre + ".json", features)
 
 if __name__ == '__main__':
 
     playlist = {
         "rock": [
-            "37i9dQZF1DWZJhOVGWqUKF", # https://open.spotify.com/playlist/37i9dQZF1DWZJhOVGWqUKF?si=d7e98ab126fc4775
-            "37i9dQZF1DX4vth7idTQch", # https://open.spotify.com/playlist/37i9dQZF1DX4vth7idTQch?si=b6615e9fe24247e0
-            "37i9dQZF1DX3oM43CtKnRV" # https://open.spotify.com/playlist/37i9dQZF1DX3oM43CtKnRV?si=22531f45d2ab4fa3
+            "37i9dQZF1DWZJhOVGWqUKF", # https://open.spotify.com/playlist/37i9dQZF1DWZJhOVGWqUKF?si=d7e98ab126fc4775 100
+            "37i9dQZF1DX4vth7idTQch", # https://open.spotify.com/playlist/37i9dQZF1DX4vth7idTQch?si=b6615e9fe24247e0 100
+            "37i9dQZF1DX3oM43CtKnRV", # https://open.spotify.com/playlist/37i9dQZF1DX3oM43CtKnRV?si=22531f45d2ab4fa3 100
+            "37i9dQZF1DX1spT6G94GFC", # https://open.spotify.com/playlist/37i9dQZF1DX1spT6G94GFC?si=da016a8b75ca4184 80
+            "37i9dQZF1DXcF6B6QPhFDv", # https://open.spotify.com/playlist/37i9dQZF1DXcF6B6QPhFDv?si=8625e0be663f4817 50
+            "37i9dQZF1DWWzBc3TOlaAV"  # https://open.spotify.com/playlist/37i9dQZF1DWWzBc3TOlaAV?si=96185faf547f4064 90
+                                                                                                                   # 520
         ],
         "edm": [
-            "37i9dQZF1DX0pH2SQMRXnC", # https://open.spotify.com/playlist/37i9dQZF1DX0pH2SQMRXnC?si=19a3ef5d30d54f06
-            "37i9dQZF1DX6J5NfMJS675", # https://open.spotify.com/playlist/37i9dQZF1DX6J5NfMJS675?si=51bc4eb90bfb45c4
-            "37i9dQZF1DX7ZUug1ANKRP" # https://open.spotify.com/playlist/37i9dQZF1DX7ZUug1ANKRP?si=ad499640a06c4936
+            "37i9dQZF1DX0pH2SQMRXnC", # https://open.spotify.com/playlist/37i9dQZF1DX0pH2SQMRXnC?si=19a3ef5d30d54f06 200
+            "37i9dQZF1DX6J5NfMJS675", # https://open.spotify.com/playlist/37i9dQZF1DX6J5NfMJS675?si=51bc4eb90bfb45c4 60
+            "37i9dQZF1DX7ZUug1ANKRP", # https://open.spotify.com/playlist/37i9dQZF1DX7ZUug1ANKRP?si=ad499640a06c4936 50
+            "6FjUyONoZCmdvKPfeWEohP", # https://open.spotify.com/playlist/6FjUyONoZCmdvKPfeWEohP?si=60e85e51ee6c4dc1 130
+            "5oJkaanfqM8upo86pAu96I" # https://open.spotify.com/playlist/5oJkaanfqM8upo86pAu96I?si=c4c9a13fbc5e4943 70
+                                                                                                                   # 510
         ],
         "classic": [
-            "37i9dQZF1DXaHEllsiT8lf", # https://open.spotify.com/playlist/37i9dQZF1DXaHEllsiT8lf?si=268afb23cad843dd
-            "37i9dQZF1DWZf52HmhYw49", # https://open.spotify.com/playlist/37i9dQZF1DWZf52HmhYw49?si=3615d6f7e736421b
-            "37i9dQZF1DWWEJlAGA9gs0" # https://open.spotify.com/playlist/37i9dQZF1DWWEJlAGA9gs0?si=e72bea12b8a74858
+            "37i9dQZF1DXaHEllsiT8lf", # https://open.spotify.com/playlist/37i9dQZF1DXaHEllsiT8lf?si=268afb23cad843dd 95
+            "37i9dQZF1DWZf52HmhYw49", # https://open.spotify.com/playlist/37i9dQZF1DWZf52HmhYw49?si=3615d6f7e736421b 50
+            "37i9dQZF1DWWEJlAGA9gs0", # https://open.spotify.com/playlist/37i9dQZF1DWWEJlAGA9gs0?si=e72bea12b8a74858 148
+            "1h0CEZCm6IbFTbxThn6Xcs", # https://open.spotify.com/playlist/1h0CEZCm6IbFTbxThn6Xcs?si=eb84a180b61940bd 128
+            "37i9dQZF1DXaHEllsiT8lf"  # https://open.spotify.com/playlist/37i9dQZF1DXaHEllsiT8lf?si=4d2de6bac7574da8 95
+                                                                                                                   # 516
         ],
         "jazz": [
-            "37i9dQZF1DWTR4ZOXTfd9K", # https://open.spotify.com/playlist/37i9dQZF1DWTR4ZOXTfd9K?si=48a1869291e54948
-            "37i9dQZF1DX1C8KR4UJlnr", # https://open.spotify.com/playlist/37i9dQZF1DX1C8KR4UJlnr?si=e2334a238c0f4557
-            "37i9dQZF1DX9n1kQRulpEn" # https://open.spotify.com/playlist/37i9dQZF1DX9n1kQRulpEn?si=c3ff641894514280
+            "37i9dQZF1DWTR4ZOXTfd9K", # https://open.spotify.com/playlist/37i9dQZF1DWTR4ZOXTfd9K?si=48a1869291e54948 100
+            "37i9dQZF1DX1C8KR4UJlnr", # https://open.spotify.com/playlist/37i9dQZF1DX1C8KR4UJlnr?si=e2334a238c0f4557 100
+            "37i9dQZF1DX9n1kQRulpEn", # https://open.spotify.com/playlist/37i9dQZF1DX9n1kQRulpEn?si=c3ff641894514280 100
+            "37i9dQZF1DWYxwmBaMqxsl", # https://open.spotify.com/playlist/37i9dQZF1DWYxwmBaMqxsl?si=c8716c64f78f45bd 78
+            "37i9dQZF1DX2iVTU1bf67i"  # https://open.spotify.com/playlist/37i9dQZF1DX2iVTU1bf67i?si=8ba59c02886449fc 150
+                                                                                                                   # 528
+          
         ],
         "hiphop": [
-            "37i9dQZF1DX7Mq3mO5SSDc", # https://open.spotify.com/playlist/37i9dQZF1DX7Mq3mO5SSDc?si=3ea682942a2a4734
-            "37i9dQZF1DX186v583rmzp", # https://open.spotify.com/playlist/37i9dQZF1DX186v583rmzp?si=f87582055c5245cf
-            "0weizyV5WNZP3tvfXWVfmg" # https://open.spotify.com/playlist/0weizyV5WNZP3tvfXWVfmg?si=904f5a0e453248dd
+            "37i9dQZF1DX7Mq3mO5SSDc", # https://open.spotify.com/playlist/37i9dQZF1DX7Mq3mO5SSDc?si=3ea682942a2a4734 50
+            "37i9dQZF1DX186v583rmzp", # https://open.spotify.com/playlist/37i9dQZF1DX186v583rmzp?si=f87582055c5245cf 100
+            "5TZkls9cEOzWDR6qCxwDot", # https://open.spotify.com/playlist/5TZkls9cEOzWDR6qCxwDot?si=d3cf9c440f9244fe 168
+            "3A46xM92rYMDL7Ohezea0H"  # https://open.spotify.com/playlist/3A46xM92rYMDL7Ohezea0H?si=b543916164544a71 216
+                                                                                                                   # 534
         ]
     }
 
